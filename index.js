ea0d8ca2721e6d9188b7662d45dd2b34b6df4885
tfuquirk <timothyfquirk@gmail.com>
2019-02-08 16:06:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(date) {
+    let currentYear = new Date(date, 0).getYear()
+    return currentYear - this.startDate.getYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(beginningLocation, endingLocation) {
+    let eastWest = ['1st Avenue',
+                    '2nd Avenue',
+                    '3rd Avenue',
+                    'Lexington Avenue',
+                    'Park',
+                    'Madison Avenue',
+                    '5th Avenue'
+                  ];
+    const northSouthDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    const eastWestDistance = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+
+    return northSouthDistance + eastWestDistance
+  }
+
+  estimatedTime(peak=false) {
+    if (peak === false) {
+      return this.blocksTravelled() / 3
+    }
+    else {
+      return this.blocksTravelled() / 2
+    }
+  }
+}
